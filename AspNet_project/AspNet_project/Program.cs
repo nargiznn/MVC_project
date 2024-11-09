42b62074634f789f87b9fa540b830ea0e73e4b32
Nergiz Nadirli <nergiznadirli@Nergiz-Air.bbrouter>
2024-11-09 04:45:44 +0400

    admin and index but some problem

--- AspNet_project/AspNet_project/Program.cs
+++ AspNet_project/AspNet_project/Program.cs
@@ -6,9 +6,13 @@ builder.Services.AddControllersWithViews();
 //builder.Services.AddAutoMapper(typeof(Program));
 //builder.Services.AddDbContext<AppDbContext>(options =>
 //  options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
 var app = builder.Build();
-
-
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "Admin",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
