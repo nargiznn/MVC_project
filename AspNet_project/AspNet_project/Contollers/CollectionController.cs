07fd411a2e16f9238e669fe439ab27aba5e72a44
Nergiz Nadirli <nergiznadirli@Nergiz-Air.bbrouter>
2024-11-13 14:54:01 +0400

    collection page short(price ,a-z) and paginetedlist

--- AspNet_project/AspNet_project/Contollers/CollectionController.cs
+++ AspNet_project/AspNet_project/Contollers/CollectionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AspNet_project.Data;
+using AspNet_project.Models;
 using AspNet_project.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,19 +21,18 @@ namespace AspNet_project.Contollers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string size, string priceRange)
+        public async Task<IActionResult> Index(string size, string priceRange, string sortBy, int page = 1, int pageSize = 6)
         {
             var productsQuery = _context.Products
                 .Include(m => m.ProductImages)
                 .Include(m => m.ProductTypes)
-                .Where(p => p.DiscountPrice > 0) 
                 .AsQueryable();
             if (!string.IsNullOrEmpty(priceRange))
             {
                 switch (priceRange.ToLower())
                 {
                     case "under100":
-                        productsQuery = productsQuery.Where(p => (p.Price < 100 || p.DiscountPrice < 100));
+                        productsQuery = productsQuery.Where(p => ((p.Price < 100 || p.DiscountPrice < 100 )) && (p.DiscountPrice !=0));
                         break;
                     case "100to300":
                         productsQuery = productsQuery.Where(p => (p.Price >= 100 && p.Price <= 300) || (p.DiscountPrice >= 100 && p.DiscountPrice <= 300));
@@ -50,14 +50,37 @@ namespace AspNet_project.Contollers
                         break;
                 }
             }
-            var products = await productsQuery.ToListAsync();
+            switch (sortBy)
+            {
+                case "alphabetically-az":
+                    productsQuery = productsQuery.OrderBy(p => p.Title);
+                    break;
+                case "alphabetically-za":
+                    productsQuery = productsQuery.OrderByDescending(p => p.Title);
+                    break;
+                case "price-low-to-high":
+                    productsQuery = productsQuery.OrderBy(p => p.Price);
+                    break;
+                case "price-high-to-low":
+                    productsQuery = productsQuery.OrderByDescending(p => p.Price);
+                    break;
+                case "best-selling":
+                    productsQuery = productsQuery.OrderByDescending(p => p.SalesCount);
+                    break;
+                default:
+                    productsQuery = productsQuery.OrderBy(p => p.Title); 
+                    break;
+            }
+
+            var paginatedProducts = await PaginatedList<Product>.CreateAsync(productsQuery, page, pageSize);
 
 
             return View(new CollectionVM
             {
-                Products = products,
+                Products = paginatedProducts,
                 ProductImages = await _context.ProductImages.ToListAsync(),
                 TypeModels = await _context.TypeModels.ToListAsync(),
+                PriceRange = priceRange,
             });
         }
 
--- AspNet_project/AspNet_project/ViewModels/CollectionVM.cs
+++ AspNet_project/AspNet_project/ViewModels/CollectionVM.cs
@@ -3,12 +3,16 @@ using AspNet_project.Models;
 
 namespace AspNet_project.ViewModels
 {
-	public class CollectionVM
-	{
-        public List<Product> Products { get; set; }
+    public class CollectionVM
+    {
+        public PaginatedList<Product> Products { get; set; }
         public List<TypeModel> TypeModels { get; set; }
         public List<ProductImage> ProductImages { get; set; }
+        public string PriceRange { get; set; }
 
+
+      
+        
     }
 }
 
